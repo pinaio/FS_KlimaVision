dfaf1ac3d010adb082d3800a2b6223d1d6f0a080
pinaio <52298091+pinaio@users.noreply.github.com>
2023-02-08 16:38:45 +0100

    Json-Server-Anbindung und filterbare Tabelle

--- src/views/DataView.vue
+++ src/views/DataView.vue
@@ -1,4 +1,26 @@
-<script setup></script>
+<script setup>
+import { computed, onMounted, ref } from "vue";
+
+const laender = ref([]);
+const search = ref("");
+
+const searchedLaender = computed(() => {
+  return laender.value.filter((land) =>
+    land.Land.toLowerCase().includes(search.value.toLowerCase())
+  );
+});
+
+onMounted(() => {
+  fetch("http://localhost:3000/data")
+    .then((res) => res.json())
+    .then((data) => (laender.value = data))
+    .catch((err) => console.log(err.message));
+});
+
+function showIt() {
+  console.log(laender.value[1]);
+}
+</script>
 <template>
   <div>
     <div class="min-h-36 flex flex-col justify-center bg-red-200 md:flex-col">
@@ -16,14 +38,23 @@
     <div class="flex flex-col items-center justify-between">
       <!-- Wrapper um die Steuerelemente-->
       <div class="my- 4 flex w-full justify-around bg-slate-200 py-4">
-        <button type="button" class="rounded-lg bg-emerald-400 px-6 py-4">
+        <button
+          type="button"
+          @click="showIt"
+          class="rounded-lg bg-emerald-400 px-6 py-4"
+        >
           Sortieren
         </button>
-        <input type="search" placeholder="Suchen" class="p-2" />
+        <input
+          type="search"
+          v-model="search"
+          placeholder="Suchen"
+          class="p-2"
+        />
       </div>
       <div class="py-6">
         <table
-          class="mx-2 border-collapse rounded-md bg-gradient-to-tr from-slate-900 to-emerald-600 p-1 text-center md:mx-auto"
+          class="mx-2 table-fixed border-collapse rounded-md bg-gradient-to-tr from-slate-900 to-emerald-600 p-1 text-center md:mx-auto md:table-fixed"
         >
           <thead class="text-amber-100">
             <tr class="">
@@ -35,24 +66,18 @@
               </th>
             </tr>
           </thead>
-          <tr class="bg-amber-50 font-medium text-emerald-900">
-            <td class="border border-gray-700">Deutschland</td>
-            <td class="border border-gray-700">454654</td>
-            <td class="border border-gray-700">4545</td>
-            <td class="border border-gray-700">13%</td>
-          </tr>
-          <tr class="bg-amber-50 font-medium text-emerald-900">
-            <td class="border border-gray-700">China</td>
-            <td class="border border-gray-700">454654</td>
-            <td class="border border-gray-700">4545</td>
-            <td class="border border-gray-700">13%</td>
-          </tr>
-          <tr class="bg-amber-50 font-medium text-emerald-900">
-            <td class="border border-gray-700">Russland</td>
-            <td class="border border-gray-700">454654</td>
-            <td class="border border-gray-700">4545</td>
-            <td class="border border-gray-700">13%</td>
-          </tr>
+          <tbody v-for="land in searchedLaender" :key="land.Land">
+            <tr class="bg-amber-50 font-medium text-emerald-900">
+              <td class="border border-gray-700">{{ land.Land }}</td>
+              <td class="border border-gray-700">
+                {{ Math.round(land.co2Emmision) }}
+              </td>
+              <td class="border border-gray-700">
+                {{ Math.round(land.proKopf) }}
+              </td>
+              <td class="border border-gray-700">13%</td>
+            </tr>
+          </tbody>
         </table>
       </div>
     </div>
