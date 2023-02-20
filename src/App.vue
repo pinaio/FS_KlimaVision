86493cca8763b1140e58b1ab1218f7e1a98caee1
pinaio <p.wagner@pinaio.de>
2023-02-20 18:18:17 +0100

    fixed the blog-reload bug with a :key property in the router view

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
+import { RouterLink, RouterView, useRoute } from "vue-router";
 import MainNav from "./components/MainNav.vue";
 import Footer from "./components/Footer.vue";
+
+const route = useRoute();
 </script>
 
 <template>
   <MainNav />
-  <RouterView />
+  <RouterView :key="route.fullPath" />
   <Footer />
 </template>
 <script></script>
