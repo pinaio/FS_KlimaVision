ea870274494f01178d25829dc59f8ba752d24caa
pinaio <52298091+pinaio@users.noreply.github.com>
2023-02-21 19:01:45 +0100

    added a key to Routerview

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
+import { RouterLink, RouterView, useRoute } from "vue-router";
 import MainNav from "./components/MainNav.vue";
 import Footer from "./components/Footer.vue";
+const route = useRoute();
 </script>
 
 <template>
   <MainNav />
-  <RouterView />
+  <RouterView :key="route.fullPath" />
   <Footer />
 </template>
+<script></script>
 
 <style src="..\src\assets\base.css"></style>
