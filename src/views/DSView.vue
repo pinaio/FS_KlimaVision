829f63f92afd53050125a40010100282227ccd1a
pinaio <52298091+pinaio@users.noreply.github.com>
2023-06-24 16:29:24 +0200

    Update of the final files

---
+++ src/views/DSView.vue
@@ -0,0 +1,69 @@
+<script setup></script>
+
+<template>
+  <div class="my-16 flex min-h-screen">
+    <div class="mx-auto flex max-w-3xl flex-col space-y-16">
+      <h1 class="text-3xl font-bold text-green-600">Datenschutzerklärung</h1>
+      <p>
+        Datenschutzerklärung Verantwortlich im Sinne der Datenschutzgesetzes:
+        Datenschutz Als Webseitenbetreiber nehmen wir den Schutz aller
+        persönlichen Daten sehr ernst. Alle personenbezogenen Informationen
+        werden vertraulich und gemäß den gesetzlichen Vorschriften behandelt,
+        wie in dieser Datenschutzerklärung erläutert. Unserer Webseite kann
+        selbstverständlich genutzt werden, ohne dass Sie persönliche Daten
+        angeben. Wenn jedoch zu irgendeinem Zeitpunkt persönliche Daten wie z.B.
+        Name, Adresse oder E-Mail abgefragt werden, wird dies auf freiwilliger
+        Basis geschehen. Niemals werden von uns erhobene Daten ohne Ihre
+        spezielle Genehmigung an Dritte weitergegeben. Datenübertragung im
+        Internet, wie zum Beispiel über E-Mail, kann immer Sicherheitslücken
+        aufweisen. Der komplette Schutz der Daten ist im Internet nicht möglich.
+      </p>
+      <p>
+        Auskunft, Löschung, Sperrung Zu jedem Zeitpunkt können Sie sich über die
+        personenbezogenen Daten, deren Herkunft und Empfänger und den Nutzen der
+        Datenverarbeitung informieren und unentgeltlich eine Korrektur, Sperrung
+        oder Löschung dieser Daten verlangen. Bitte nutzen Sie dafür die im
+        Impressum angegebenen Kontaktwege. Für weitere Fragen zum Thema stehen
+        wir Ihnen ebenfalls jederzeit zur Verfügung. Server-Log-Files Der
+        Seiten-Provider erhebt und speichert automatisch Daten in Server-Log
+        Files, die von Ihrem Browser an uns übermittelt werden. Diese Daten
+        enthalten: – Browsertyp/ Browserversion – Betriebssystem des Rechners –
+        Referrer URL – Hostname des zugreifenden Rechners – Uhrzeit der
+        Serveranfrage Diese Daten sind nicht personenbezogen. Es erfolgt keine
+        Zusammenführung dieser Daten mit anderen Datenquellen. Wenn uns konkrete
+        Anhaltspunkte für eine rechtswidrige Nutzung bekannt werden behalten wir
+        uns das Recht vor, diese Daten nachträglich zu überprüfen. Cookies Viele
+        Internetseiten verwenden Cookies. Cookies sind unschädlich für Ihren
+        Rechner und virenfrei. Sie dienen dazu, Internet-Angebote für die
+        Besucher einer Webseite freundlicher, effektiver und sicherer zu machen.
+        Cookies sind kleine Textdateien, die auf Ihrem Computer abgelegt werden
+        und die Ihr Browser verwendet. Wir verwenden in der Regel so genannte
+        „Session-Cookies“. Diese werden nach Verlassen unserer Seite automatisch
+        gelöscht. Andere Cookies bleiben auf Ihrem Computer gespeichert, bis Sie
+        diese löschen. Diese Cookies helfen dabei, Ihren Rechner beim nächsten
+        Besuch wiederzuerkennen. Über die Browsereinstellungen können sie
+        festlegen, dass Sie über neue Cookies informiert werden und Cookies
+        jeweils annehmen müssen. Ebenso können Sie die Annahme von Cookies für
+        bestimmte Fälle oder generell ausschließen oder das automatische Löschen
+        der Cookies beim Schließen des Browser aktivieren.
+      </p>
+      <p>
+        >Werden Cookies deaktiviert, kann die Funktionalität unserer Website
+        eingeschränkt sein. Änderung der Datenschutzbestimmungen Unsere
+        Datenschutzerklärung können in unregelmäßigen Abständen angepasst
+        werden, damit sie den aktuellen rechtlichen Anforderungen entsprechen
+        oder um Änderungen unserer Dienstleistungen umzusetzen, z. B. bei der
+        Einfügung neuer Angebote. Für Ihren nächsten Besuch gilt dann
+        automatisch die neue Datenschutzerklärung. Kontakt zum
+        Datenschutzmitarbeiter Für Fragen zum Datenschutz schicken Sie uns bitte
+        eine Nachricht an mit dem Betreff „Datenschutz“.
+      </p>
+    </div>
+  </div>
+</template>
+
+<style>
+p {
+  @apply decoration-green-600 decoration-solid decoration-4 underline-offset-4;
+}
+</style>
